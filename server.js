d8609a2d8958d2cc17234b7a1ed3598c96e22fbd
shaikhejaz226-cmd <shaikhejaz226@gmail.com>
2025-11-26 11:00:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,5 +17,25 @@ const LIVEKIT_SERVER_URL = 'wss://YOUR_SERVER_URL';
 const TWILIO_ACCOUNT_SID = 'ACf5e63d7bfe662c8be625ef78815b9161'; 
 const TWILIO_AUTH_TOKEN = '94c39e1c48db77980e572495e435b741';                       
 const TWILIO_PHONE_NUMBER = '+16185239920'; 
-
+//TOKEN generation
 const twilioClient = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+//Token generation
+app.get('/api/livekit/token', (req, res) => {
+    const { roomName, identity, role } = req.query; 
+
+    const at = new AccessToken(LIVEKIT_API_KEY, LIVEKIT_API_SECRET, {
+        identity: identity,
+        ttl: '10m',
+    });
+
+    const grant = { roomJoin: true, room: roomName };
+    if (role === 'publisher') {
+        grant.canPublish = true; // User can publish video/audio
+    } else if (role === 'subscriber') {
+        grant.canSubscribe = true; // Viewer can only watch
+    }
+    at.addGrant(grant);
+
+    const token = at.toJwt();
+    res.send({ token, serverUrl: LIVEKIT_SERVER_URL });
+});
